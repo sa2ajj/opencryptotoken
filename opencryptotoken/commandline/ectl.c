6b54048321383e04e0f0285a05578aa4016481a1
krynski <krynski@bae76c23-6f1e-0410-9410-e57fb4223014>
2007-09-07 00:07:42 +0000

    test openssl signature verification

--- opencryptotoken/commandline/ectl.c
+++ opencryptotoken/commandline/ectl.c
@@ -8,12 +8,16 @@
 /*
 General Description:
 */
+#include <openssl/x509.h>
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <usb.h>    /* this is libusb, see http://libusb.sourceforge.net/ */
 #include "../firmware/ec.h"
+
+//#include <openssl/ossl_typ.h>
+
 
 #define USBDEV_SHARED_VENDOR    0x16C0  /* VOTI */
 #define USBDEV_SHARED_PRODUCT   0x05DC  /* Obdev's free shared PID */
@@ -134,11 +138,18 @@ static int          didUsbInit = 0;
     return errorCode;
 }
 
-void hex_print(FILE *f,unsigned char *buf, int len) {
+void hex_print_num(FILE *f,unsigned char *buf, int len) {
   int i;
    for(i=0; i<len; i++) {
      fprintf(f,"%02x",buf[len-i-1]);
    }
+   fflush(f); 
+}
+
+void hex_print(FILE *f,unsigned char *buf, int len) {
+   int i;
+   for(i=0; i<len; i++) 
+     fprintf(f,"%02x",*buf++);
    fflush(f); 
 }
 
@@ -154,6 +165,33 @@ int hex2bin(char *arg,unsigned char *buf) {
     p--;
   }
   return (n+1)/2;
+}
+
+int read_cert(usb_dev_handle *handle,unsigned char *result,int maxlen) {
+        int total=0;
+        int nBytes,ret;
+        ret=usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, AVRCMD_SELECT_FILE, 0, 0, 0, 0, 5000);
+        if(ret<0) return ret;
+        
+        while(total<maxlen && (nBytes=usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, AVRCMD_READ_BINARY, total, total+AVR_FILE_BUF_SIZE<=maxlen ? AVR_FILE_BUF_SIZE : maxlen-total, result+total, AVR_FILE_BUF_SIZE, 5000))>0) {
+          fprintf(stdout,"read %d bytes, total:%d\n",nBytes,total);
+          total+=nBytes;
+        }
+        if(nBytes<0) return nBytes;
+        return total;
+}
+
+ECDSA_SIG *sig2SIG(ecdsa_sig_t *sig) {
+  ECDSA_SIG *s=ECDSA_SIG_new();
+  ecdsa_sig_t copy;
+  int i;
+  for(i=0; i<N; i++) {
+    copy.R.value[i]=sig->R.value[N-1-i];
+    copy.S.value[i]=sig->S.value[N-1-i];
+  }
+  BN_bin2bn(copy.R.value,N,s->r);
+  BN_bin2bn(copy.S.value,N,s->s);
+  return s;
 }
 
 int main(int argc, char **argv)
@@ -186,9 +224,9 @@ int                 nBytes;
         while((nBytes=usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, AVRCMD_GETRESULT, 0, 0, (char *)buf, 256, 5000))==0 && n++<25)
           usleep(200000);
         if(nBytes==3*N) {
-          fprintf(stdout,"X:"); hex_print(stdout,buf,N);
-          fprintf(stdout,"\nY:"); hex_print(stdout,buf+N,N);
-          fprintf(stdout,"\nZ:"); hex_print(stdout,buf+2*N,N);
+          fprintf(stdout,"X:"); hex_print_num(stdout,buf,N);
+          fprintf(stdout,"\nY:"); hex_print_num(stdout,buf+N,N);
+          fprintf(stdout,"\nZ:"); hex_print_num(stdout,buf+2*N,N);
           fprintf(stdout,"\n");
         } else {
           fprintf(stderr,"mult timeout\n");
@@ -198,29 +236,51 @@ int                 nBytes;
         memset(buf,0,256);
         nBytes=usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, AVRCMD_GETRESULT, 0, 0, (char *)buf, 256, 5000);
         if(nBytes==3*N) {
-          fprintf(stdout,"X:"); hex_print(stdout,buf,N);
-          fprintf(stdout,"\nY:"); hex_print(stdout,buf+N,N);
-          fprintf(stdout,"\nZ:"); hex_print(stdout,buf+2*N,N);
+          fprintf(stdout,"X:"); hex_print_num(stdout,buf,N);
+          fprintf(stdout,"\nY:"); hex_print_num(stdout,buf+N,N);
+          fprintf(stdout,"\nZ:"); hex_print_num(stdout,buf+2*N,N);
           fprintf(stdout,"\n");
         } else {
           fprintf(stderr,"err:%d\n",nBytes);
         }
-    } else if(strcmp(argv[1], "sign") == 0) {
-        nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE, AVRCMD_SIGN, 0, 0, argv[2], strlen(argv[2]), 5000);
-        int n=0;
+    } else if(strcmp(argv[1], "sign") == 0 && argc>2) {
+        char *dgst=argv[2];
+        int dgst_len=strlen(dgst);
+        EC_KEY *eckey=0;
+        unsigned char result[2048];
+        unsigned char *p;
+        nBytes=read_cert(handle,result,sizeof(result));
+        if(nBytes>=0) {
+          X509 *cert=0;
+          p=result;
+          d2i_X509(&cert,&p,nBytes);
+          if(cert) {
+            EVP_PKEY * evpKey = X509_get_pubkey(cert);
+            if(evpKey != NULL) 
+              eckey = EVP_PKEY_get1_EC_KEY(evpKey);
+          }
+        } else {
+          fprintf(stderr,"read cert error\n");
+        }
+        fprintf(stdout,"signing %d bytes\n",dgst_len);
+        nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE, AVRCMD_SIGN, 0, 0, dgst, dgst_len, 5000);
         unsigned char buf[256];
         memset(buf,0,256);
         ecdsa_sig_t sig;
-        usleep(3*1000000);
-;        while((nBytes=usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, AVRCMD_GETRESULT, 0, 0, (char *)&sig, sizeof(ecdsa_sig_t), 5000))==0 && n++<25)
-;          usleep(200000);
+//        usleep(1000000);
+        nBytes=usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, AVRCMD_GETRESULT, 0, 0, (char *)&sig, sizeof(ecdsa_sig_t), 5000);
         if(nBytes==sizeof(ecdsa_sig_t)) {
-          fprintf(stdout,"R:"); hex_print(stdout,sig.R.value,N);
-          fprintf(stdout,"\nS:"); hex_print(stdout,sig.S.value,N);
-          fprintf(stdout,"\n");
+          ECDSA_SIG *s = sig2SIG(&sig);
+          fprintf(stdout,"r:%s\ns:%s\n",BN_bn2hex(s->r),BN_bn2hex(s->s));
+          if(ECDSA_do_verify(dgst,dgst_len,s,eckey)) {
+            fprintf(stdout,"sig verified\n");
+          } else {
+            fprintf(stdout,"sig not verified\n");
+          }
         } else {
           fprintf(stderr,"sign timeout, nb:%d\n",nBytes);
         }
+    } else if(strcmp(argv[1], "cert") == 0) {
     } else if(strcmp(argv[1], "rnd") == 0) {
       unsigned char buf[32];
       nBytes=usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, AVRCMD_GETRND, 0, 0, (char *)buf, 32, 5000);
@@ -228,22 +288,24 @@ int                 nBytes;
 //      hex_print(stdout,buf,nBytes);
 //      fprintf(stdout,"\n");
     } else if(strcmp(argv[1], "test") == 0){
-        int i, v, r;
+        int i, v1, r1,v2,r2;
 /* The test consists of writing 1000 random numbers to the device and checking
  * the echo. This should discover systematic bit errors (e.g. in bit stuffing).
  */
         for(i=0;i<1000;i++){
-            v = rand() & 0xffff;
-            nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, PSCMD_ECHO, v, 0, (char *)buffer, sizeof(buffer), 5000);
-            if(nBytes < 2){
+            v1 = rand() & 0xffff;
+            v2 = rand() & 0xffff;
+            nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, PSCMD_ECHO, v1, v2, (char *)buffer, sizeof(buffer), 5000);
+            if(nBytes < 4){
                 if(nBytes < 0)
                     fprintf(stderr, "USB error: %s\n", usb_strerror());
                 fprintf(stderr, "only %d bytes received in iteration %d\n", nBytes, i);
                 exit(1);
             }
-            r = buffer[0] | (buffer[1] << 8);
-            if(r != v){
-                fprintf(stderr, "data error: received 0x%x instead of 0x%x in iteration %d\n", r, v, i);
+            r1 = buffer[0] | (buffer[1] << 8);
+            r2 = buffer[2] | (buffer[3] << 8);
+            if(r1 != v1 || r2 != v2){
+                fprintf(stderr, "data error: received %x %x instead of %x %x in iteration %d\n", r1,r2, v1,v2, i);
                 exit(1);
             }
         }
